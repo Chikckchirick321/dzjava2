0e909f108239b197daebcbbec4d604a9c24089ca
Memotest <mov3st@yandex.ru>
2023-05-22 12:27:03 +0300

    src changed 3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket_cost = 17546;
-        int rubles_in_one_mile = 20;
-        int number_of_numbered_miles_purchased_ticket = ticket_cost/rubles_in_one_mile;
+        int TicketCost = 17546;
+        int RublesInOneMile = 20;
+        int NumberOfNumberedMilesPurchasedTicket = TicketCost / RublesInOneMile;
 
-                System.out.println("For this flight will be credited: " + number_of_numbered_miles_purchased_ticket + "miles");
+        System.out.println("For this flight will be credited: " + NumberOfNumberedMilesPurchasedTicket + "miles");
         // Объявляете переменные для входных данных и
         // параметров программы: одну для хранения
         // стоимости билета, другую для хранения количества
